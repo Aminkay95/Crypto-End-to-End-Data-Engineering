22804cb71b98f8f7caf13d9eceb1459ef2262c91
Aminkay95 <mohamedamino882@gmail.com>
2025-03-22 22:16:56 +0300

    incoporated push to s3 function

--- dags/dags.py
+++ dags/dags.py
@@ -1,29 +1,30 @@
 import requests
 import json
 import datetime
-#from env import keys
+import boto3
 from websocket import create_connection, WebSocketConnectionClosedException
 
 
-def extract_coinbase()
-    ws = create_connection('wss://ws-feed.exchange.coinbase.com')
 
-    ws. send(
-        json.dumps(
-            {
-                'type':'subscribe',
-                'product_ids':['BTC-USD','ETH-USD','SOL-USD'],
-                'channels':['matches']
-                
-            }
-        )
+ws = create_connection('wss://ws-feed.exchange.coinbase.com')
+
+ws. send(
+    json.dumps(
+        {
+            'type':'subscribe',
+            'product_ids':['BTC-USD','ETH-USD','SOL-USD'],
+            'channels':['matches']
+            
+        }
     )
+)
 
 
 
+while True:
     data = json.loads(ws.recv())
-    data1 = json.dumps(data)
-    with open('coinbase_prices.json','w') as file:
-        file.write(data1)
+
+
+
 
     
